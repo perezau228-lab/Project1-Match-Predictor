235922701c1ac5c148674c6d494d27517d532acc
Anibal Perez <perezau228@gmail.com>
2026-09-22 09:26:50 -0400

    Update fotmob.client.py

--- fotmob.client.py
+++ fotmob.client.py
@@ -5,7 +5,9 @@ class FotMobClient():
   def __init__(self):
     self.base_url = "https://www.fotmob.com"
   def get_match(self, match_id):
-    pass
+    response = requests.get(f"{self.base_url}/api/MatchDetails", params={'match_id'})
+    response.raise_for_status()
+    return response.json()
   def get_team(self, team_id):
     pass
   def get_fixtures(self, league_id):
