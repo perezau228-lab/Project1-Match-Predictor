ed67dbcc50913c847bed69817210ceab9618a616
Anibal Perez <perezau228@gmail.com>
2026-09-22 09:37:41 -0400

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,1 +1,14 @@
+class Precictor():
+  def predict(self, home_team, away_team):
 
+    home_strength = self.calculate_strength(home_team)
+    away_strength = self.calculate_strength(away_team)
+
+    total = home_strenght + away_strength
+
+    home_probability = home_strength / total
+    away_probability = away_strength / total
+
+    draw_probability = 1 - home_probability - away_probability
+
+    return{"home","draw","away"}
