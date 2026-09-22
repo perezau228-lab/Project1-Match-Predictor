942ac9515c9150e42623ea28c305f32218eff421
Anibal Perez <perezau228@gmail.com>
2026-09-22 09:22:25 -0400

    Fix BASE_URL to instance variable self.base_url

---
+++ fotmob.client.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+class FotMobClient():
+  def __init__(self):
+    self.base_url = "https://www.fotmob.com"
+  def get_match(self, match_id):
+    pass
+  def get_team(self, team_id):
+    pass
+  def get_fixtures(self, league_id):
+    pass
