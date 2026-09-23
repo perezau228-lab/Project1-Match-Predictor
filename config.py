0889f9f47d21f48e48e1f1a9e7db78b91bd4b553
Anibal Perez <perezau228@gmail.com>
2026-09-23 11:17:46 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-import sqlite3
-
-class DataBase:
-  def __init__(self):
-    self.connection = sqlite3.connect("soccer.db")
-  def sav_match(self, match):
-    pass
-  def get_team_history(self, team_id):
-    pass
+League = ""
+Update_Interval = 30
+Datebase_Name = "soccer.db"
